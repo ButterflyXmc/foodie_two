fd000890a076129df2bfa25fcd4a33467a7708f9
mahic <mahic@domain.com>
2023-02-21 17:39:27 -0700

    Organizing

--- apihelper.py
+++ apihelper.py
@@ -1,3 +1,5 @@
+# Aware of of what this function does, but might not use it just now...
+
 def check_info(sent_data, required_data):
     for data in required_data:
         # if a user misses to fill out the field
