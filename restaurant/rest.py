ad77460ae0328efe778696f18d234f354efbf37c
mahic <mahic@domain.com>
2023-02-21 18:15:44 -0700

    fixed app.py

--- restaurant/rest.py
+++ restaurant/rest.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from helpers.dbhelpers import run_statement
 from dbcreds import production_mode
+import client.client as c
 
 
 app = Flask(__name__)
